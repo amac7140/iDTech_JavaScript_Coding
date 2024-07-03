39336fbffd74fdf67a502dbc6b351cae1d375802
Aimee MacDonald <amac714@MacBook-Air.local>
2024-07-03 11:13:21 -0500

    adding all project files

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,30 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greeting = "Welcome to my ice cream store! "
+let name = "Aimee"
+let shopName = "Fat Cat Creamery"
+let title = "cashier"
+// TODO: Create 3-Number variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 18
+let height = 53
+let year = 2024
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let iceCreamFlavors = ["Vanilla", "Chocolate", "Strawberry", "Cookies and Cream"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let yummy = true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greeting + "My name is " + name + ".")
+console.log("I'm the " + title + " at " + shopName + ".")
+console.log("I am " + age + " and " + height + " inches tall.")
+console.log("I started working here in " + year + ".")
+console.log("Does anyone have any questions?")
+console.log("somebody else: Do you have a favorite ice cream flavor?")
+console.log("Me: " + yummy + ". My favorite flavor is " + iceCreamFlavors[3] + ".")
+console.log("Feel free to sample any flavors if you would like!")
+
